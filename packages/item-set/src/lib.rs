61876de87a4440ca918037878d4d6edf5a4b571f
larry <26318510+larry0x@users.noreply.github.com>
2022-12-06 02:48:45 +0000

    move unit tests into src folder

--- packages/item-set/src/lib.rs
+++ packages/item-set/src/lib.rs
@@ -17,7 +17,8 @@ use cw_storage_plus::{Bound, Prefix, Prefixer};
 
 /// A set of non-duplicate items.
 ///
-/// This implementation is equivalent to storing these items as keys in a `Map<T, Empty>`.
+/// On a high level, a `Set<T>` is equivalent to a `Map<T, Empty>`, but offers
+/// a more intuitive API similar to native HashSet and BTreeSet.
 pub struct Set<'a, T> {
     namespace: &'a [u8],
 
@@ -184,5 +185,196 @@ where
 
         #[cfg(feature = "counter")]
         self.counter.remove(store);
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+// Tests
+//--------------------------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "iterator")]
+    use std::ops::Range;
+    use cosmwasm_std::testing::MockStorage;
+    use super::*;
+
+    const NAMESPACE: &str = "names";
+
+    #[cfg(not(feature = "counter"))]
+    const NAMES: Set<&str> = Set::new(NAMESPACE);
+    #[cfg(feature = "counter")]
+    const NAMES: Set<&str> = Set::new(NAMESPACE, "names__counter");
+
+    #[cfg(all(feature = "iterator", not(feature = "counter")))]
+    const TUPLES: Set<(u64, &str)> = Set::new("tuples");
+    #[cfg(all(feature = "iterator", feature = "counter"))]
+    const TUPLES: Set<(u64, &str)> = Set::new("tuples", "tuples__counter");
+
+    /// Returns the raw storage key for a given name.
+    /// The key is: length of namespace (2 bytes) + namespace + the name
+    fn key(name: &str) -> Vec<u8> {
+        let length_bytes = (NAMESPACE.len() as u32).to_be_bytes();
+        let mut out = Vec::with_capacity(2 + NAMESPACE.len() + name.len());
+        out.extend_from_slice(&[length_bytes[2], length_bytes[3]]);
+        out.extend_from_slice(NAMESPACE.as_bytes());
+        out.extend_from_slice(name.as_bytes());
+        out
+    }
+
+    /// Return a list of mockup names for use in testing.
+    #[cfg(feature = "iterator")]
+    fn mock_names(indexes: Range<usize>) -> Vec<String> {
+        let mut names = indexes.map(|i| format!("test-name-{i}")).collect::<Vec<_>>();
+        names.sort();
+        names
+    }
+
+    /// Insert mock names into a set.
+    #[cfg(feature = "iterator")]
+    fn insert_mock_names(set: Set<&str>, store: &mut dyn Storage) {
+        for name in mock_names(1..100) {
+            set.insert(store, &name).unwrap();
+        }
+    }
+
+    #[test]
+    fn containing() {
+        let mut store = MockStorage::default();
+
+        NAMES.insert(&mut store, "larry").unwrap();
+        assert!(NAMES.contains(&store, "larry"));
+        assert!(!NAMES.contains(&store, "jake"));
+
+        NAMES.insert(&mut store, "jake").unwrap();
+        assert!(NAMES.contains(&store, "larry"));
+        assert!(NAMES.contains(&store, "jake"));
+    }
+
+    #[test]
+    fn inserting() {
+        let mut store = MockStorage::default();
+
+        let new = NAMES.insert(&mut store, "larry").unwrap();
+        assert!(new);
+        assert_eq!(store.get(&key("larry")), Some(b"{}".to_vec()));
+        assert_eq!(store.get(&key("jake")), None);
+
+        let new = NAMES.insert(&mut store, "larry").unwrap();
+        assert!(!new);
+        assert_eq!(store.get(&key("larry")), Some(b"{}".to_vec()));
+        assert_eq!(store.get(&key("jake")), None);
+
+        let new = NAMES.insert(&mut store, "jake").unwrap();
+        assert!(new);
+        assert_eq!(store.get(&key("larry")), Some(b"{}".to_vec()));
+        assert_eq!(store.get(&key("jake")), Some(b"{}".to_vec()));
+    }
+
+    #[test]
+    fn removing() {
+        let mut store = MockStorage::default();
+
+        NAMES.insert(&mut store, "larry").unwrap();
+
+        let existed = NAMES.remove(&mut store, "larry").unwrap();
+        assert!(existed);
+        assert_eq!(store.get(&key("larry")), None);
+
+        let existed = NAMES.remove(&mut store, "jake").unwrap();
+        assert!(!existed);
+        assert_eq!(store.get(&key("jake")), None);
+    }
+
+    #[cfg(feature = "counter")]
+    #[test]
+    fn counting() {
+        let mut store = MockStorage::default();
+
+        let count = NAMES.count(&store).unwrap();
+        assert_eq!(count, 0);
+
+        NAMES.insert(&mut store, "larry").unwrap();
+        assert_eq!(NAMES.count(&store).unwrap(), 1);
+
+        NAMES.insert(&mut store, "jake").unwrap();
+        assert_eq!(NAMES.count(&store).unwrap(), 2);
+
+        NAMES.insert(&mut store, "pumpkin").unwrap();
+        assert_eq!(NAMES.count(&store).unwrap(), 3);
+
+        #[cfg(feature = "iterator")]
+        {
+            NAMES.clear(&mut store);
+            assert_eq!(NAMES.count(&store).unwrap(), 0);
+        }
+    }
+
+    #[cfg(feature = "iterator")]
+    #[test]
+    fn iterating() {
+        let mut store = MockStorage::default();
+
+        insert_mock_names(NAMES, &mut store);
+
+        let names = NAMES
+            .items(&store, None, None, Order::Ascending)
+            .collect::<StdResult<Vec<_>>>()
+            .unwrap();
+        assert_eq!(names, mock_names(1..100));
+
+        let start_after = Bound::ExclusiveRaw(b"test-name-2".to_vec());
+        let names = NAMES
+            .items(&store, Some(start_after), None, Order::Ascending)
+            .take(10)
+            .collect::<StdResult<Vec<_>>>()
+            .unwrap();
+        assert_eq!(names, mock_names(20..30));
+    }
+
+    #[cfg(feature = "iterator")]
+    #[test]
+    fn clearing() {
+        let mut store = MockStorage::default();
+
+        insert_mock_names(NAMES, &mut store);
+
+        NAMES.clear(&mut store);
+
+        let names = NAMES
+            .items(&store, None, None, Order::Ascending)
+            .collect::<StdResult<Vec<_>>>()
+            .unwrap();
+        assert_eq!(names.len(), 0);
+    }
+
+    #[cfg(feature = "iterator")]
+    #[test]
+    fn prefixes() {
+        let mut store = MockStorage::default();
+
+        let tuples = vec![
+            (1u64, "larry"),
+            (1u64, "jake"),
+            (2u64, "pumpkin"),
+        ];
+
+        for tuple in &tuples {
+            TUPLES.insert(&mut store, *tuple).unwrap();
+        }
+
+        let names = TUPLES
+            .prefix(1)
+            .keys(&store, None, None, Order::Ascending)
+            .collect::<StdResult<Vec<_>>>()
+            .unwrap();
+        assert_eq!(names, vec!["jake", "larry"]);
+
+        let names = TUPLES
+            .prefix(2)
+            .keys(&store, None, None, Order::Ascending)
+            .collect::<StdResult<Vec<_>>>()
+            .unwrap();
+        assert_eq!(names, vec!["pumpkin"]);
     }
 }
