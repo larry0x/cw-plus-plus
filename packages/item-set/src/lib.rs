9b749a9ac57144a0c4572d37dc0ef04ee9bc4be2
larry <26318510+larry0x@users.noreply.github.com>
2022-12-06 02:26:56 +0000

    use readme as docs

--- packages/item-set/src/lib.rs
+++ packages/item-set/src/lib.rs
@@ -1,40 +1,4 @@
-//! Set of non-duplicate items for [CosmWasm](https://github.com/CosmWasm/cosmwasm) smart contract
-//! store.
-//!
-//! ## Usage
-//!
-//! ```rust
-//! use cosmwasm_std::{DepsMut, Order, StdResult};
-//! use cw_item_set::Set;
-//!
-//! const VALIDATORS: Set<&str> = Set::new("validators", "validators__counter");
-//!
-//! fn example(deps: DepsMut) -> StdResult<()> {
-//!     // add a new item to the set
-//!     VALIDATORS.insert(deps.storage, "larry")?;
-//!
-//!     // remove an existing item from the set
-//!     VALIDATORS.remove(deps.storage, "jake")?;
-//!
-//!     // check whether an item is in the set
-//!     let is_whitelisted = VALIDATORS.contains(deps.as_ref().storage, "pumpkin");
-//!
-//!     // check the total number of of items in the set
-//!     let num_validators = VALIDATORS.count(deps.as_ref().storage)?;
-//!
-//!     // enumerate items in the set
-//!     VALIDATORS
-//!         .items(deps.as_ref().storage, None, None, Order::Ascending)
-//!         .for_each(|validator| {
-//!             println!("{} is whitelisted!", validator);
-//!         });
-//!
-//!     // delete all items in the set
-//!     VALIDATORS.clear(deps.storage);
-//!
-//!     Ok(())
-//! }
-//! ```
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 
 use std::marker::PhantomData;
 
